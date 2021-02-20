7f0e66925d73c7825c74957c36edbe1b18a2b98e
Pawel Piskorz <ppiskorz0@gmail.com>
2021-02-20 17:38:05 +0100

    changes in CMakeLists_main.txt - changes allow to add more than one target to one CMakeLists.txt file

--- src/CComponentCreator/__about__.py
+++ src/CComponentCreator/__about__.py
@@ -9,7 +9,7 @@ __all__ = ('__title__', '__summary__', '__uri__', '__version_info__',
 __title__        = "CComponentCreator"
 __summary__      = "Python package for creating components in C language"
 __uri__          = "https://github.com/zxvcv/CComponentCreator"
-__version_info__ = type("version_info", (), dict(major=0, minor=0, micro=3,
+__version_info__ = type("version_info", (), dict(major=0, minor=0, micro=4,
                         releaselevel="final", serial=0))
 __version__      = "{0.major}.{0.minor}.{0.micro}{1}{2}".format(__version_info__,
                    dict(alpha="a", beta="b", candidate="rc", final="",
